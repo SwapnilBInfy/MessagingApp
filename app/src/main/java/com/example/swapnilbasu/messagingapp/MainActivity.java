adc3a5ecdddaf9bdb0dfd864382cef92aec5afee
Swapnil Basu <swapnil.basu@ad.infosys.com>
2018-03-30 17:29:48 -0400

    Initial commit

---
+++ app/src/main/java/com/example/swapnilbasu/messagingapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.swapnilbasu.messagingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editMessage;
+    private DatabaseReference mDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editMessage = (EditText) findViewById(R.id.editMessageE);
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Messages");
+
+
+    }
+
+    public void sendButtonClicked(View view) {
+        final String messageValue = editMessage.getText().toString().trim();
+        if(!TextUtils.isEmpty(messageValue)){
+            final DatabaseReference newPost = mDatabase.push();
+            newPost.child("content").setValue(messageValue);
+        }
+    }
+}
